c5e02ebde616f827ef71c7d5f27b743ec94c02f0
Julia Canzani <julia@teamtangible.com>
2025-06-14 09:51:23 -0400

    Update agent image, add user fallback image, resize image in authentication dummy data to match

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,6 @@ export const authenticateUser = async () => {
   return {
     customerID: "e8f9g0h1-2i3j-4k5l-6m7n-8o9p0q1r2s3t",
     name: "Jaden",
-    image: "https://placehold.co/60x60?text=J"
+    image: "https://placehold.co/256x256?text=J"
   };
 }
