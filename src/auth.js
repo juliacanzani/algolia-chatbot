5fa4d8095e6394fe1abbaf6d5213148f56ed6ae6
Julia Canzani <julia@teamtangible.com>
2025-06-15 10:17:23 -0400

    Apply theme colours to placeholder images

--- src/auth.js
+++ src/auth.js
@@ -3,17 +3,17 @@ export const authenticateUser = async (req) => {
     jaden: {
       customerID: "e8f9g0h1-2i3j-4k5l-6m7n-8o9p0q1r2s3t",
       name: "Jaden",
-      image: "https://placehold.co/256x256?text=J"
+      image: "https://placehold.co/256x256/E2F2F4/4BB8A7?text=J"
     },
     alex: {
       customerID: "a2v3w4x5-6z7a-8b9c-d0e1-f2g3h4i5j6k7",
       name: "Alex",
-      image: "https://placehold.co/256x256?text=A"
+      image: "https://placehold.co/256x256/E2F2F4/4BB8A7?text=A"
     },
     riley: {
       customerID: "a3f2b8c9-55c0-4d7d-9874-4b8f8c9d1a2b",
       name: "Riley",
-      image: "https://placehold.co/256x256?text=R"
+      image: "https://placehold.co/256x256/E2F2F4/4BB8A7?text=R"
     }
   };
 
